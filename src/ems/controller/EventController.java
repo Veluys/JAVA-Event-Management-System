aef60a57731160f159ebf5c3ec3a474a39d0b251
Vel <meisterrabyte@gmail.com>
2025-11-11 20:13:04 +0800

    Add addEvent method

--- src/ems/controller/EventController.java
+++ src/ems/controller/EventController.java
@@ -1,4 +1,31 @@
 package ems.controller;
 
+import ems.model.EventDAO;
+import ems.view.Displayer;
+
+import java.util.LinkedHashMap;
+
 public class EventController {
+    Displayer displayer = new Displayer();
+    InputGetter inputGetter = new InputGetter();
+
+    private final String[] event_attributes = {"Event Name", "Date", "Start Time", "End Time", "Venue"};
+    private void addEvent(){
+        LinkedHashMap<String, String> event = new LinkedHashMap<>();
+
+        for(String eventDetailName : event_attributes){
+            if(eventDetailName.equals("Date")){
+                displayer.showPrompt(eventDetailName + "(Ex: January 1, 2001): ");
+                event.put("Date", String.format("'%s'", inputGetter.getDate()));
+            }else if(eventDetailName.contains("Time")){
+                displayer.showPrompt(eventDetailName + "(Ex. 9:00 a.m.): ");
+                event.put(eventDetailName, String.format("'%s'", inputGetter.getTime()));
+            }else{
+                displayer.showPrompt(eventDetailName + ": ");
+                event.put(eventDetailName, String.format("'%s'", inputGetter.getLine()));
+            }
+        }
+        String eventDetails = String.join(",", event.values());
+        EventDAO.insert(eventDetails);
+    }
 }
