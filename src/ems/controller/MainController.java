10f03deb0decc5f91eb1bd90d8047a5ddd655cb0
Vel <meisterrabyte@gmail.com>
2025-11-11 19:01:02 +0800

    Add prompt for main menu

--- src/ems/controller/MainController.java
+++ src/ems/controller/MainController.java
@@ -5,9 +5,17 @@ import ems.view.Displayer;
 public class MainController {
     public static void execute(){
         Displayer displayer = new Displayer();
+        InputGetter inputGetter = new InputGetter();
+
         displayer.displayHeader("Welcome to Event Management System");
 
         String[] mainMenuOptions = {"Events", "Registration", "Participants", "Attendance", "Exit"};
         displayer.showMenu("What do you want to do or work with today?", mainMenuOptions);
+
+        int option;
+        do{
+            displayer.numberedMenuPrompt();
+            option = inputGetter.getPositiveInt(mainMenuOptions.length);
+        }while(option == -1);
     }
 }
