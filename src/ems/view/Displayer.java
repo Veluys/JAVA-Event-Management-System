e7aa2e94a87923447f9cdec5515a8fbe02718595
Vel <meisterrabyte@gmail.com>
2025-11-11 20:23:38 +0800

    Move the methods of EntityDisplayer class to Displayer

--- src/ems/view/Displayer.java
+++ src/ems/view/Displayer.java
@@ -28,4 +28,31 @@ public class Displayer {
     public void numberedMenuPrompt(){
         showPrompt("Enter the number of your option: ");
     }
+
+    public void centerAlignRow(String[] columnValues) {
+        final int tableWidth = displayWidth - 4;
+        final int columnWidth = tableWidth / 4;
+
+        for (int i = 0; i < columnValues.length; i++) {
+            String columnValue = columnValues[i];
+            int totalPadding = columnWidth - columnValue.length();
+            int paddingStart = totalPadding / 2;
+            int paddingEnd = totalPadding - paddingStart;
+
+            String centered = " ".repeat(paddingStart) + columnValue + " ".repeat(paddingEnd);
+            System.out.print(centered + "|");
+        }
+        System.out.println();
+        System.out.println("-".repeat(120));
+    }
+
+    public void rightAlignRecord(String[] columnValues){
+        final int labelWidth = 15;
+        String attributeLabel = columnValues[0];
+        String attributeValue = columnValues[1];
+
+        int paddingRight = labelWidth - attributeLabel.length();
+
+        System.out.println(attributeLabel + ":" + " ".repeat(paddingRight) + attributeValue);
+    }
 }
