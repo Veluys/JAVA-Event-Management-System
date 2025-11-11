6cbf3eaac3b17468fdf6d9f6dc1e274c2ee7c72a
Vel <meisterrabyte@gmail.com>
2025-11-11 19:03:37 +0800

    Add Connection and Event class

---
+++ src/ems/model/DBConnection.java
@@ -0,0 +1,23 @@
+package ems.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    Connection getConnection(){
+        final String username = "postgres";
+        final String password = "byte";
+        final String url = "jdbc:postgresql://localhost:5432/event_mgmt";
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println("An unexpected error has occurred!");
+            System.out.printf("Error: %s \n", e.getMessage());
+        }
+
+        return connection;
+    }
+}
