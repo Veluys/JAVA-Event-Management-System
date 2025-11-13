00cd1d5d0a8bb5ac24391bc1b0201e8512a06dfc
Vel <meisterrabyte@gmail.com>
2025-11-13 16:20:34 +0800

    Add eventSelection method

--- src/ems/controller/RegController.java
+++ src/ems/controller/RegController.java
@@ -1,13 +1,23 @@
 package ems.controller;
 
+import ems.model.EventDAO;
 import ems.view.Displayer;
+
+import java.util.LinkedHashMap;
 
 public class RegController {
     Displayer displayer = new Displayer();
     InputGetter inputGetter = new InputGetter();
 
     public void execute(){
-        EventMenu();
+        if(EventDAO.getLatestEventId() == 0){
+            System.out.println("There are no events yet!");
+            return;
+        }
+
+        int selected_event_id = eventSelection();
+
+        participantMenu();
         int option;
 
         do{
@@ -29,9 +39,21 @@ public class RegController {
         System.out.println();
     }
 
-    private void EventMenu(){
+    private void participantMenu(){
         displayer.displayHeader("Registration");
         String[] operations = {"Add Participant", "View Participants", "Search Participant", "Remove Participants", "Exit"};
         displayer.showMenu("Select an operation:", operations);
+    }
+
+    private int eventSelection(){
+        displayer.displayHeader("Registration");
+        int event_id;
+
+        do{
+            displayer.showPrompt("Enter event id: ");
+            event_id = inputGetter.getPositiveInt(EventDAO.getLatestEventId());
+        }while (!EventDAO.eventExist("event_id = '" + event_id + "'"));
+
+        return event_id;
     }
 }
