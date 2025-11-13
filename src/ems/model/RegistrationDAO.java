01825730f952901bc9a3d0e03cd3727fbaa2d1ac
Vel <meisterrabyte@gmail.com>
2025-11-13 14:56:25 +0800

    Add insert method

--- src/ems/model/RegistrationDAO.java
+++ src/ems/model/RegistrationDAO.java
@@ -1,7 +1,26 @@
 package ems.model;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class RegistrationDAO {
     final static Connection connection = DBConnection.getConnection();
+
+    public static void insert(String values){
+        String insertQuery = String.format("INSERT INTO registration (event_id, participant_id) " +
+                "VALUES (%s)", values);
+
+        try{
+            Statement insertStatement = connection.createStatement();
+
+            if(insertStatement.executeUpdate(insertQuery) == 1){
+                System.out.println("Insert operation successful");
+            }else{
+                throw new SQLException();
+            }
+        }catch (SQLException e){
+            System.out.println("Insert operation unsuccessful!");
+        }
+    }
 }
