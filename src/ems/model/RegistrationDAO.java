d981dac398a1e2befc4094a96ce9b0dd8a785c96
Vel <meisterrabyte@gmail.com>
2025-11-13 15:10:11 +0800

    Add show method

--- src/ems/model/RegistrationDAO.java
+++ src/ems/model/RegistrationDAO.java
@@ -1,8 +1,10 @@
 package ems.model;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class RegistrationDAO {
     final static Connection connection = DBConnection.getConnection();
@@ -21,6 +23,37 @@ public class RegistrationDAO {
             }
         }catch (SQLException e){
             System.out.println("Insert operation unsuccessful!");
+        }
+    }
+
+    public static ArrayList<ArrayList<String>> show(){
+        String[] columns = {"registration_id", "participant_id", "last_name", "first_name", "has_attended"};
+        String selectQuery = "SELECT r.registration_id, r.participant_id, p.last_name, p.first_name, r.has_attended " +
+                             "FROM registration AS r " +
+                             "LEFT JOIN participants AS p " +
+                                "ON r.participant_id = p.participant_i " +
+                             "ORDER BY registration_id DESC;";
+
+        ArrayList<ArrayList<String>> events = new ArrayList<>();
+
+        try{
+            Statement stmt = connection.createStatement();
+            ResultSet eventsSet = stmt.executeQuery(selectQuery);
+
+            if(!eventsSet.next()) return null;
+            do{
+                ArrayList<String> event = new ArrayList<>();
+
+                for(String column : columns){
+                    event.add(eventsSet.getString(column));
+                }
+                events.add(event);
+            } while(eventsSet.next());
+
+            return events;
+        }catch (SQLException e){
+            System.out.println("SELECT operation unsuccessful!");
+            return null;
         }
     }
 }
