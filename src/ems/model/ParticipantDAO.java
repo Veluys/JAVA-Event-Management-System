f6e85793f038c86c8e28c8fd2d90ad48a300b6f5
Vel <meisterrabyte@gmail.com>
2025-11-17 15:27:15 +0800

    Add ParticipantDAO class

---
+++ src/ems/model/ParticipantDAO.java
@@ -0,0 +1,26 @@
+package ems.model;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ParticipantDAO {
+    final static Connection connection = DBConnection.getConnection();
+
+    public static void insert(String values){
+        String insertQuery = String.format("INSERT INTO participants (dept_id, last_name, first_name) " +
+                "VALUES (%s)", values);
+
+        try{
+            Statement insertStatement = connection.createStatement();
+
+            if(insertStatement.executeUpdate(insertQuery) == 1){
+                System.out.println("Insert operation successful");
+            }else{
+                throw new SQLException();
+            }
+        }catch (SQLException e){
+            System.out.println("Insert operation unsuccessful!");
+        }
+    }
+}
