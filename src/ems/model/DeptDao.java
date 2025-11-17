2caa5464b0b0bf2f8ec2bb551f6cd345fc00a968
Vel <meisterrabyte@gmail.com>
2025-11-17 15:12:48 +0800

    Add DeptDao class

---
+++ src/ems/model/DeptDao.java
@@ -0,0 +1,66 @@
+package ems.model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DeptDao {
+    final static Connection connection = DBConnection.getConnection();
+
+    public static ArrayList<String> getDeptShortNames(){
+        String selectQuery = "SELECT dept_shortname " +
+                "FROM department " +
+                "ORDER BY dept_id";
+
+        ArrayList<String> venueNames = new ArrayList<>();
+
+        try{
+            Statement stmt = connection.createStatement();
+            ResultSet venueSet = stmt.executeQuery(selectQuery);
+
+            if(!venueSet.next()) return null;
+
+            do{
+                venueNames.add(venueSet.getString("dept_shortname"));
+            } while(venueSet.next());
+
+            return venueNames;
+        }catch (SQLException e){
+            System.out.println("SELECT operation unsuccessful!");
+            return null;
+        }
+    }
+
+    public static int getLatestDeptId(){
+        String countQuery = "SELECT COALESCE(MAX(dept_id), 0) AS latest_dept_id FROM department";
+
+        try{
+            Statement stmt = connection.createStatement();
+            ResultSet resultSet = stmt.executeQuery(countQuery);
+
+            resultSet.next();
+            return resultSet.getInt("latest_dept_id");
+        }catch (SQLException e){
+            return -1;
+        }
+    }
+
+    public static int getDeptId(String dept_shortname){
+        String searchQuery = String.format("SELECT dept_id FROM venues WHERE dept_shortname = '%s'", dept_shortname);
+
+        try{
+            Statement eventStatement = connection.createStatement();
+            ResultSet eventResult = eventStatement.executeQuery(searchQuery);
+
+            if(!eventResult.next()){
+                return -1;
+            }
+
+            return eventResult.getInt("dept_id");
+        }catch (SQLException e){
+            return -1;
+        }
+    }
+}
