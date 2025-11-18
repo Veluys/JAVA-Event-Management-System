f877ff8cc6fbb60a8246afaed8ed0272506ebea1
Vel <meisterrabyte@gmail.com>
2025-11-18 21:04:56 +0800

    Add AttendanceDAO class

---
+++ src/ems/model/AttendanceDAO.java
@@ -0,0 +1,45 @@
+package ems.model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class AttendanceDAO {
+    final static Connection connection = DBConnection.getConnection();
+
+    public static ArrayList<ArrayList<String>> show(int event_id){
+        String[] columns = {"participant_id", "dept_shortname", "last_name", "first_name"};
+        String selectQuery = "SELECT p.participant_id, dept_shortname, last_name, first_name " +
+                             "FROM participants AS p " +
+                             "INNER JOIN registration AS r " +
+                             "  ON p.participant_id = r.participant_id " +
+                             "  AND r.attended = true " +
+                             "  AND r.event_id = " + event_id + " " +
+                             "INNER JOIN departments AS d " +
+                             "  ON p.dept_id = d.dept_id";
+
+        ArrayList<ArrayList<String>> events = new ArrayList<>();
+
+        try{
+            Statement stmt = connection.createStatement();
+            ResultSet eventsSet = stmt.executeQuery(selectQuery);
+
+            if(!eventsSet.next()) return null;
+            do{
+                ArrayList<String> event = new ArrayList<>();
+
+                for(String column : columns){
+                    event.add(eventsSet.getString(column));
+                }
+                events.add(event);
+            } while(eventsSet.next());
+
+            return events;
+        }catch (SQLException e){
+            System.out.println("SELECT operation unsuccessful!");
+            return null;
+        }
+    }
+}
