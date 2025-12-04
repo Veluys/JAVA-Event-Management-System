ea76758ca2d34af564c1abec9f844f7eea2f6824
Vel <meisterrabyte@gmail.com>
2025-12-04 09:16:44 +0800

    Add constructors, setters, and getters

--- src/ems/model/Registration.java
+++ src/ems/model/Registration.java
@@ -1,4 +1,20 @@
 package ems.model;
 
 public class Registration {
+    private int event_id;
+    private String sr_code;
+    private boolean attended;
+
+    public Registration(int event_id, String sr_code){
+        this.event_id = event_id;
+        this.sr_code = sr_code;
+    }
+
+    public void set_event_id(int event_id) {this.event_id = event_id;}
+    public void set_sr_code(String sr_code) {this.sr_code = sr_code;}
+    public void set_attended(boolean attended) {this.attended = attended;}
+
+    public int get_event_id() {return event_id;}
+    public String get_sr_code() {return sr_code;}
+    public boolean get_attended() {return attended;}
 }
